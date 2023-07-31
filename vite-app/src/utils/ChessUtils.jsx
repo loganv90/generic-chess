cae6edc7285f25159627f15751aef913092ec797
Logan Vaughan <logan.v90@gmail.com>
2023-07-30 18:30:28 -0700

    adding board class and coding pawn moves

--- vite-app/src/utils/ChessUtils.jsx
+++ vite-app/src/utils/ChessUtils.jsx
@@ -1,20 +1,203 @@
-function getPieceMoves(squareId, piece, squareConfigs, xSquares, ySquares) {
-    return []
-    // if (piece.name === 'p') {
-    //     return getPawnMoves(squareId, piece, squareConfigs)
-    // } else if (piece.name === 'n') {
-    //     return getKnightMoves(squareId, piece, squareConfigs)
-    // } else if (piece.name === 'b') {
-    //     return getBishopMoves(squareId, piece, squareConfigs)
-    // } else if (piece.name === 'r') {
-    //     return getRookMoves(squareId, piece, squareConfigs)
-    // } else if (piece.name === 'q') {
-    //     return getQueenMoves(squareId, piece, squareConfigs)
-    // } else if (piece.name === 'k') {
-    //     return getKingMoves(squareId, piece, squareConfigs)
-    // } else {
-    //     return []
-    // }
+function createPieceFromFen(c) {
+    switch (c) {
+        case 'r':
+            return new Rook('black')
+        case 'n':
+            return new Knight('black')
+        case 'b':
+            return new Bishop('black')
+        case 'q':
+            return new Queen('black')
+        case 'k':
+            return new King('black')
+        case 'p':
+            return new Pawn('black', 0, 1)
+        case 'R':
+            return new Rook('white')
+        case 'N':
+            return new Knight('white')
+        case 'B':
+            return new Bishop('white')
+        case 'Q':
+            return new Queen('white')
+        case 'K':
+            return new King('white')
+        case 'P':
+            return new Pawn('white', 0, -1)
+        default:
+            return null
+    }
 }
 
-export default getPieceMoves
+function createSquaresFromFen(fenPieces) {
+    const squareRows = []
+    const rows = fenPieces.split('/')
+
+    for (const row of rows) {
+        const squares = []
+
+        for (const c of row) {
+            let x = squares.length
+            let y = squareRows.length
+            let color = x + y % 2 == 0 ? 'white' : 'black'
+
+            if (isNaN(c)) {
+                squares.push(new Square(color, createPieceFromFen(c)))
+            } else {
+                squares.push(...Array(parseInt(c)).fill(new Square(color)))
+            }
+        }
+
+        squareRows.push(squares)
+    }
+
+    return squareRows
+}
+
+class Board {
+    constructor(
+        player = 'white',
+        fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+    ) {
+        const fenSplit = fen.split(' ')
+
+        if (fenSplit.length != 6) {
+            throw new Error('Invalid FEN string')
+        }
+
+        this.player = player == 'white' ? 0 : 1
+        this.currentPlayer = fenSplit[1] == 'w' ? 0 : 1
+        this.players = ['white', 'black']
+        this.halfMoveClock = parseInt(fenSplit[4])
+        this.fullMoveNumber = parseInt(fenSplit[5])
+        this.squares = createSquaresFromFen(fenSplit[0])
+    }
+
+    getPieceMoves(x, y) {
+        const piece = this.squares[y][x].piece
+        return piece ? piece.constructor.getPieceMoves(this.squares, x, y) : []
+    }
+}
+
+class Square {
+    constructor(color, piece = null, isSelected = false, isDestination = false) {
+        this.color = color
+        this.isSelected = isSelected
+        this.isDestination = isDestination
+        this.piece = piece
+    }
+}
+
+class Piece {
+    constructor(color, name) {
+        this.color = color
+        this.name = name
+    }
+
+    static getPieceMoves() {
+        throw new Error('getPieceMoves() not implemented')
+    }
+}
+
+class Pawn extends Piece {
+    constructor(color, xDir, yDir, moved = false, enPassant = null) {
+        super(color, 'p')
+        this.xDir = xDir
+        this.yDir = yDir
+        this.moved = moved
+        this.enPassant = enPassant
+    }
+
+    static getPieceMoves(squares, x, y) {
+        const pawn = squares[y][x].piece
+        const moves = []
+        const xMin = 0
+        const xMax = squares[0].length - 1
+        const yMin = 0
+        const yMax = squares.length - 1
+
+
+        const straights = []
+        const diagonals = []
+        if (pawn.xDir > 0) {
+            if (x+1<=xMax) { straights.push({x: x+1, y: y}) }
+            if (!pawn.moved && x+2<=xMax) { straights.push({x: x+2, y: y}) }
+            if (x+1<=xMax && y+1<=yMax) { diagonals.push({x: x+1, y: y+1, xSide: x, ySide: y+1}) }
+            if (x+1<=xMax && y-1>=yMin) { diagonals.push({x: x+1, y: y-1, xSide: x, ySide: y-1}) }
+        } else if (pawn.xDir < 0) {
+            if (x-1>=xMin) { straights.push({x: x-1, y: y}) }
+            if (!pawn.moved && x-2>=xMin) { straights.push({x: x-2, y: y}) }
+            if (x-1>=xMin && y+1<=yMax) { diagonals.push({x: x-1, y: y+1, xSide: x, ySide: y+1}) }
+            if (x-1>=xMin && y-1>=yMin) { diagonals.push({x: x-1, y: y-1, xSide: x, ySide: y-1}) }
+        } else if (pawn.yDir > 0) {
+            if (y+1<=yMax) { straights.push({x: x, y: y+1}) }
+            if (!pawn.moved && y+2<=yMax) { straights.push({x: x, y: y+2}) }
+            if (x+1<=xMax && y+1<=yMax) { diagonals.push({x: x+1, y: y+1, xSide: x+1, ySide: y}) }
+            if (x-1>=xMin && y+1<=yMax) { diagonals.push({x: x-1, y: y+1, xSide: x-1, ySide: y}) }
+        } else if (pawn.yDir < 0) {
+            if (y-1>=yMin) { straights.push({x: x, y: y-1}) }
+            if (!pawn.moved && y-2>=yMin) { straights.push({x: x, y: y-2}) }
+            if (x+1<=xMax && y-1>=yMin) { diagonals.push({x: x+1, y: y-1, xSide: x+1, ySide: y}) }
+            if (x-1>=xMin && y-1>=yMin) { diagonals.push({x: x-1, y: y-1, xSide: x-1, ySide: y}) }
+        }
+
+        for (const straight of straights) {
+            if (!squares[straight.y][straight.x].piece) {
+                moves.push({x: straight.x, y: straight.y})
+            } else {
+                break
+            }
+        }
+
+        for (const diagonal of diagonals) {
+            if (squares[diagonal.y][diagonal.x].piece
+                && squares[diagonal.y][diagonal.x].piece.color != pawn.color) {
+                moves.push({x: diagonal.x, y: diagonal.y})
+            } else if (squares[diagonal.ySide][diagonal.xSide].piece
+                && squares[diagonal.ySide][diagonal.xSide].piece.color != pawn.color
+                && squares[diagonal.ySide][diagonal.xSide].piece.name == 'p'
+                && squares[diagonal.ySide][diagonal.xSide].piece.enPassant.x == diagonal.x
+                && squares[diagonal.ySide][diagonal.xSide].piece.enPassant.y == diagonal.y) {
+                moves.push({x: diagonal.x, y: diagonal.y})
+            }
+        }
+
+        return moves
+    }
+}
+
+class Knight extends Piece {
+    constructor(color) {
+        super(color, 'n')
+    }
+}
+
+class Bishop extends Piece {
+    constructor(color) {
+        super(color, 'b')
+    }
+}
+
+class Rook extends Piece {
+    constructor(color, moved = false) {
+        super(color, 'r')
+        this.moved = moved
+    }
+}
+
+class Queen extends Piece {
+    constructor(color) {
+        super(color, 'q')
+    }
+}
+
+class King extends Piece {
+    constructor(color, moved = false, inCheck = false, inCheckmate = false) {
+        super(color, 'k')
+        this.moved = moved
+        this.inCheck = inCheck
+        this.inCheckmate = inCheckmate
+    }
+}
+
+export default Board
--- vite-app/tests/utils/ChessUtils.test.jsx
+++ vite-app/tests/utils/ChessUtils.test.jsx
@@ -1,10 +1,68 @@
 import { describe, it, expect } from "vitest"
-import getPieceMoves from "../../src/utils/ChessUtils"
+import Board from "../../src/utils/ChessUtils"
 
-describe('getPieceMoves', () => {
-    it('should return an empty array', () => {
-        expect(getPieceMoves()).toEqual([])
+describe('Board', () => {
+    it('should have default piece arrangement with no arguments', () => {
+        const board = new Board()
+        expect(board.player).toBe(0)
+        expect(board.currentPlayer).toBe(0)
+        expect(board.players).toStrictEqual(['white', 'black'])
+        expect(board.halfMoveClock).toBe(0)
+        expect(board.fullMoveNumber).toBe(1)
+        for (let i = 0; i < 8; i++) {
+            expect(board.squares[1][i].piece.name).toBe('p')
+            expect(board.squares[1][i].piece.color).toBe('black')
+            expect(board.squares[0][i].piece.color).toBe('black')
+            expect(board.squares[6][i].piece.name).toBe('p')
+            expect(board.squares[6][i].piece.color).toBe('white')
+            expect(board.squares[7][i].piece.color).toBe('white')
+            expect(board.squares[2][i].piece).toBe(null)
+            expect(board.squares[3][i].piece).toBe(null)
+            expect(board.squares[4][i].piece).toBe(null)
+            expect(board.squares[5][i].piece).toBe(null)
+        }
+        for (let i = 0; i < 8; i+=7) {
+            expect(board.squares[i][0].piece.name).toBe('r')
+            expect(board.squares[i][7].piece.name).toBe('r')
+            expect(board.squares[i][1].piece.name).toBe('n')
+            expect(board.squares[i][6].piece.name).toBe('n')
+            expect(board.squares[i][2].piece.name).toBe('b')
+            expect(board.squares[i][5].piece.name).toBe('b')
+            expect(board.squares[i][3].piece.name).toBe('q')
+            expect(board.squares[i][4].piece.name).toBe('k')
+        }
+    })
+
+    it('should throw error with invalid FEN string', () => {
+        expect(() => {
+            new Board('', '')
+        }).toThrow('Invalid FEN string')
+    })
+
+    it('getPieceMoves() should return correct moves for unmoved pawn', () => {
+        const board = new Board()
+        expect(board.getPieceMoves(4, 6)).toStrictEqual([{x: 4, y: 5}, {x: 4, y: 4}])
+    })
+
+    it('getPieceMoves() should return correct moves for moved pawn', () => {
+        const board = new Board()
+        board.squares[6][4].piece.moved = true
+        expect(board.getPieceMoves(4, 6)).toStrictEqual([{x: 4, y: 5}])
+    })
+
+    it('getPieceMoves() should return correct moves for pawn en passant', () => {
+        const board = new Board('white', 'rnbqkbnr/pppppppp/8/3pP3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+        board.squares[3][4].piece.moved = true
+        board.squares[3][3].piece.moved = true
+        board.squares[3][3].piece.enPassant = {x: 3, y: 2}
+
+        expect(board.getPieceMoves(4, 3)).toStrictEqual([{x:4, y:2}, {x:3, y:2}])
+    })
+
+    it('getPieceMoves() should return correct moves for pawn capture', () => {
+        const board = new Board('white', 'rnbqkbnr/pppppppp/3p4/4P3/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+        board.squares[3][4].piece.moved = true
+        board.squares[2][3].piece.moved = true
+        expect(board.getPieceMoves(4, 3)).toStrictEqual([{x:4, y:2}, {x:3, y:2}])
     })
 })
-
-export default getPieceMoves
