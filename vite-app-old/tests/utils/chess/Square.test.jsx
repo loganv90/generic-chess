68c0bbf88f05afaabef0523fa5096c35ddb4b82d
Logan Vaughan <logan.v90@gmail.com>
2023-09-17 16:25:59 -0700

    removing old vite app

--- vite-app-old/tests/utils/chess/Square.test.jsx
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'vitest'
-import { Square } from '/src/utils/chess/Square'
-
-describe('Square', () => {
-    it('should be able to be created', () => {
-        new Square('0-0', 0, 0, true, 'r')
-    })
-})
