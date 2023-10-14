606b13d92185430e43e424ebd126772350d938c9
Logan Vaughan <logan.v90@gmail.com>
2023-10-14 15:55:10 -0600

    finishing pawn tests

--- go-app/chess/piece_test.go
+++ go-app/chess/piece_test.go
@@ -46,3 +46,77 @@ func TestPawnMovesWhenUnmoved(t *testing.T) {
 	board.AssertExpectations(t)
 	moveFactory.AssertExpectations(t)
 }
+
+func TestPawnMovesWhenMoved(t *testing.T) {
+	board := &mockBoard{}
+	board.On("getPiece", mock.Anything, mock.Anything).Return(nil, nil)
+	board.On("xLen").Return(8)
+	board.On("yLen").Return(8)
+	board.On("possibleEnPassants", mock.Anything, mock.Anything, mock.Anything).Return([]*enPassant{})
+
+	moveFactory := &mockMoveFactory{}
+	moveFactory.On("newSimpleMove", board, 3, 3, 3, 4).Return(nil, nil)
+
+	pawn, err := newPawn(moveFactory, "white", true, 0, 1)
+	assert.Nil(t, err)
+
+	moves := pawn.moves(board, 3, 3)
+	assert.Len(t, moves, 1)
+
+	board.AssertExpectations(t)
+	moveFactory.AssertExpectations(t)
+}
+
+func TestPawnMovesWhenCapturing(t *testing.T) {
+	board := &mockBoard{}
+	board.On("getPiece", 2, 4).Return(nil, nil)
+	board.On("getPiece", 3, 4).Return(nil, nil)
+	board.On("getPiece", 3, 5).Return(nil, nil)
+	board.On("xLen").Return(8)
+	board.On("yLen").Return(8)
+	board.On("possibleEnPassants", mock.Anything, mock.Anything, mock.Anything).Return([]*enPassant{})
+
+	moveFactory := &mockMoveFactory{}
+	moveFactory.On("newSimpleMove", board, 3, 3, 3, 4).Return(nil, nil)
+	moveFactory.On("newSimpleMove", board, 3, 3, 3, 5).Return(nil, nil)
+	moveFactory.On("newSimpleMove", board, 3, 3, 4, 4).Return(nil, nil)
+
+	pawn, err := newPawn(moveFactory, "white", false, 0, 1)
+	assert.Nil(t, err)
+
+	blackPawn, err := newPawn(moveFactory, "black", false, 0, 1)
+	assert.Nil(t, err)
+	board.On("getPiece", 4, 4).Return(blackPawn, nil)
+
+	moves := pawn.moves(board, 3, 3)
+	assert.Len(t, moves, 3)
+
+	board.AssertExpectations(t)
+	moveFactory.AssertExpectations(t)
+}
+
+func TestPawnMovesWhenCapturingEnPassant(t *testing.T) {
+	board := &mockBoard{}
+	board.On("getPiece", 2, 4).Return(nil, nil)
+	board.On("getPiece", 3, 4).Return(nil, nil)
+	board.On("getPiece", 3, 5).Return(nil, nil)
+	board.On("getPiece", 4, 4).Return(nil, nil)
+	board.On("xLen").Return(8)
+	board.On("yLen").Return(8)
+	board.On("possibleEnPassants", "white", 4, 4).Return([]*enPassant{{4, 4, 3, 4}})
+	board.On("possibleEnPassants", "white", 2, 4).Return([]*enPassant{})
+
+	moveFactory := &mockMoveFactory{}
+	moveFactory.On("newSimpleMove", board, 3, 3, 3, 4).Return(nil, nil)
+	moveFactory.On("newSimpleMove", board, 3, 3, 3, 5).Return(nil, nil)
+	moveFactory.On("newSimpleMove", board, 3, 3, 4, 4).Return(nil, nil)
+
+	pawn, err := newPawn(moveFactory, "white", false, 0, 1)
+	assert.Nil(t, err)
+
+	moves := pawn.moves(board, 3, 3)
+	assert.Len(t, moves, 3)
+
+	board.AssertExpectations(t)
+	moveFactory.AssertExpectations(t)
+}
